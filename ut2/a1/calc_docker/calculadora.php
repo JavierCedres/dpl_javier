dc7203d42c413f0856bb9bf8cf4fc884fd947d2a
JavierCedres <javmarcego@gmail.com>
2025-10-07 02:13:32 +0100

    Subido Dockerfile y default.conf

---
+++ ut2/a1/calc_docker/calculadora.php
@@ -0,0 +1,42 @@
+<link rel="stylesheet" href="./styles.css">
+<h1>Calculadora dockerizada</h1>
+<form method="post" action="">
+        <p>Valor 1: <input type="number" id="num1" name="num1"></p>
+        <p>Valor 2: <input type="number" name="num2"></p>
+        <p>Operación:
+                <select name="operacion">
+                        <option>Suma</option>
+                        <option>Resta</option>
+                        <option>Multiplicación</option>
+                        <option>División</option>
+                </select>
+	</p>
+	<img src="./calculadora.png" />
+	<br>
+	<br>
+        <button type="submit">Calcular</button>
+</form>
+<?php
+        switch($_POST["operacion"]) {
+        case "Suma":
+                $result = $_POST["num1"] + $_POST["num2"];
+                echo $result;
+                break;
+        case "Resta":
+                $result = $_POST["num1"] - $_POST["num2"];
+                echo $result;
+                break;
+        case "Multiplicación":
+                $result = $_POST["num1"] * $_POST["num2"];
+                echo $result;
+                break;
+        case "División":
+                if ($_POST["num2"] == 0) {
+                        $result = "No se puede dividir entre 0";
+                } else {
+                        $result = $_POST["num1"] / $_POST["num2"];
+                }
+                echo $result;
+                break;
+        }
+?>
