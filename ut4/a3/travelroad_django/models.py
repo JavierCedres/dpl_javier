48f6f56a0c6ce2dc72fef5a8c5a581fc4cbee6d8
JavierCedres <javmarcego@gmail.com>
2025-12-17 15:12:12 +0000

    subido proyecto django

---
+++ ut4/a3/travelroad_django/models.py
@@ -0,0 +1,233 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class Cache(models.Model):
+    key = models.CharField(primary_key=True, max_length=255)
+    value = models.TextField()
+    expiration = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'cache'
+
+
+class CacheLocks(models.Model):
+    key = models.CharField(primary_key=True, max_length=255)
+    owner = models.CharField(max_length=255)
+    expiration = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'cache_locks'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class FailedJobs(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    uuid = models.CharField(unique=True, max_length=255)
+    connection = models.TextField()
+    queue = models.TextField()
+    payload = models.TextField()
+    exception = models.TextField()
+    failed_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'failed_jobs'
+
+
+class JobBatches(models.Model):
+    id = models.CharField(primary_key=True, max_length=255)
+    name = models.CharField(max_length=255)
+    total_jobs = models.IntegerField()
+    pending_jobs = models.IntegerField()
+    failed_jobs = models.IntegerField()
+    failed_job_ids = models.TextField()
+    options = models.TextField(blank=True, null=True)
+    cancelled_at = models.IntegerField(blank=True, null=True)
+    created_at = models.IntegerField()
+    finished_at = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'job_batches'
+
+
+class Jobs(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    queue = models.CharField(max_length=255)
+    payload = models.TextField()
+    attempts = models.SmallIntegerField()
+    reserved_at = models.IntegerField(blank=True, null=True)
+    available_at = models.IntegerField()
+    created_at = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'jobs'
+
+
+class Migrations(models.Model):
+    migration = models.CharField(max_length=255)
+    batch = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'migrations'
+
+
+class PasswordResetTokens(models.Model):
+    email = models.CharField(primary_key=True, max_length=255)
+    token = models.CharField(max_length=255)
+    created_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'password_reset_tokens'
+
+
+class Places(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    visited = models.BooleanField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'places'
+
+
+class Sessions(models.Model):
+    id = models.CharField(primary_key=True, max_length=255)
+    user_id = models.BigIntegerField(blank=True, null=True)
+    ip_address = models.CharField(max_length=45, blank=True, null=True)
+    user_agent = models.TextField(blank=True, null=True)
+    payload = models.TextField()
+    last_activity = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'sessions'
+
+
+class Users(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.CharField(unique=True, max_length=255)
+    email_verified_at = models.DateTimeField(blank=True, null=True)
+    password = models.CharField(max_length=255)
+    remember_token = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'users'
