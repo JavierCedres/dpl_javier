f8aa1f1109d4122e0711a3d2b4e09f51d508b485
JavierCedres <javmarcego@gmail.com>
2026-01-16 14:58:09 +0000

    feat:Actualizada la aplicación travelroad para travelroad_django

--- ut4/a3/travelroad_django/places/views.py
+++ ut4/a3/travelroad_django/places/views.py
@@ -5,11 +5,20 @@ from .models import Place
 
 
 def index(request):
-    wished = Place.objects.filter(visited=False)
-    visited = Place.objects.filter(visited=True)
     template = loader.get_template('places/index.html')
-    context = {
-        'wished': wished,
-        'visited': visited,
-    }
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+
+def wished(request):
+    wished = Place.objects.filter(visited=False)
+    template = loader.get_template('places/wished.html')
+    context = {'wished': wished}
+    return HttpResponse(template.render(context, request))
+
+
+def visited(request):
+    visited = Place.objects.filter(visited=True)
+    template = loader.get_template('places/visited.html')
+    context = {'visited': visited}
     return HttpResponse(template.render(context, request))
